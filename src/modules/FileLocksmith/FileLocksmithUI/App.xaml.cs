12998ab6d1f097683499bf69799cb3e63b9746d9
Ivan Stošić <ivan.stosic@janeasystems.com>
2022-10-03 14:26:13 +0200

    Update UI project so it compiles

--- src/modules/FileLocksmith/FileLocksmithUI/App.xaml.cs
+++ src/modules/FileLocksmith/FileLocksmithUI/App.xaml.cs
@@ -1,51 +1,53 @@
-﻿// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+﻿// Copyright (c) Microsoft Corporation
+// The Microsoft Corporation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.Text;
+using System.Windows;
+using Common.UI;
 
 namespace FileLocksmithUI
 {
-    using Microsoft.UI.Xaml;
-    using Microsoft.UI.Xaml.Controls;
-    using Microsoft.UI.Xaml.Controls.Primitives;
-    using Microsoft.UI.Xaml.Data;
-    using Microsoft.UI.Xaml.Input;
-    using Microsoft.UI.Xaml.Media;
-    using Microsoft.UI.Xaml.Navigation;
-    using Microsoft.UI.Xaml.Shapes;
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Runtime.InteropServices.WindowsRuntime;
-    using Windows.ApplicationModel;
-    using Windows.ApplicationModel.Activation;
-    using Windows.Foundation;
-    using Windows.Foundation.Collections;
-
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    public partial class App : Application
+    public partial class App : Application, IDisposable
     {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
+        // private ThemeManager _themeManager;
+        private bool _isDisposed;
+
+        static App()
         {
-            this.InitializeComponent();
+            Console.InputEncoding = Encoding.Unicode;
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        protected override void OnStartup(StartupEventArgs e)
         {
-            m_window = new MainWindow();
-            m_window.Activate();
+            // TODO: Add command-line parameters that can be used in lieu of the input page (issue #14)
+            var mainWindow = new MainWindow();
+            mainWindow.Show();
+
+            // _themeManager = new ThemeManager(this);
         }
 
-        private Window m_window;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_isDisposed)
+            {
+                if (disposing)
+                {
+                    // _themeManager?.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                _isDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
     }
 }
